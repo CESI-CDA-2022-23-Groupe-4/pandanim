98b1677877543fe1aec50cd50c43a1e1520df1fb
François BANITZ <francois.banitz@gmail.com>
2023-01-03 19:28:00 +0100

    fix: removed home twig import

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,12 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function __construct(private $twig = new \Kenjis\CI4Twig\Twig()){}
     public function index()
     {
-        $test = 'test';
-        $this->twig->display('home/index', [
-            'test'=>$test
-        ]);
+        $this->display('home/index');
     }
 }
