1eb910ef141a13b6d05be4724a81f42308951ae7
François BANITZ <francois.banitz@gmail.com>
2023-01-05 11:31:58 +0100

    fixed table engines and added table relations

--- database/pandanim.sql
+++ database/pandanim.sql
@@ -1,33 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 05 jan. 2023 à 07:08
--- Version du serveur : 5.7.31
--- Version de PHP : 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `pandanim`
---
-CREATE DATABASE IF NOT EXISTS `pandanim` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pandanim`;
-
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           5.7.36 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.2.0.6576
 -- --------------------------------------------------------
 
---
--- Structure de la table `anime`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+CREATE DATABASE IF NOT EXISTS `pandanim` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pandanim`;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour pandanim
+CREATE DATABASE IF NOT EXISTS `pandanim` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `pandanim`;
 
 DROP TABLE IF EXISTS `anime`;
 CREATE TABLE IF NOT EXISTS `anime` (
@@ -62,9 +54,11 @@ DROP TABLE IF EXISTS `anime_genre`;
 CREATE TABLE IF NOT EXISTS `anime_genre` (
   `anime_id` smallint(5) UNSIGNED NOT NULL,
   `genre_id` smallint(5) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  KEY `anime_id` (`anime_id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `anime_genre_ibfk_1` FOREIGN KEY (`anime_id`) REFERENCES `anime` (`id`),
+  CONSTRAINT `anime_genre_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Structure de la table `anime_studio`
@@ -74,9 +68,11 @@ DROP TABLE IF EXISTS `anime_studio`;
 CREATE TABLE IF NOT EXISTS `anime_studio` (
   `anime_id` smallint(5) UNSIGNED NOT NULL,
   `studio_id` smallint(5) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  KEY `FK_anime_studio_anime` (`anime_id`),
+  KEY `FK_anime_studio_studio` (`studio_id`),
+  CONSTRAINT `FK_anime_studio_anime` FOREIGN KEY (`anime_id`) REFERENCES `anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_anime_studio_studio` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Structure de la table `genre`
@@ -101,21 +97,11 @@ CREATE TABLE IF NOT EXISTS `review` (
   `user_id` smallint(5) UNSIGNED NOT NULL,
   `score` tinyint(1) UNSIGNED NOT NULL,
   `comment` text
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `review`
---
-
-INSERT INTO `review` (`anime_id`, `user_id`, `score`, `comment`) VALUES
-(1, 1, 10, 'sqd'),
-(1, 1, 99, 'sqd'),
-(1, 1, 100, 'sqd'),
-(1, 1, 250, 'sqd'),
-(1, 1, 255, 'sqd'),
-(1, 1, 255, 'sqd');
-
--- --------------------------------------------------------
+  KEY `anime_id` (`anime_id`),
+  KEY `FK_review_user` (`user_id`),
+  CONSTRAINT `FK_review_anime` FOREIGN KEY (`anime_id`) REFERENCES `anime` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_review_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Structure de la table `studio`
@@ -126,9 +112,7 @@ CREATE TABLE IF NOT EXISTS `studio` (
   `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Structure de la table `user`
@@ -145,6 +129,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
+-- Les données exportées n'étaient pas sélectionnées.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
