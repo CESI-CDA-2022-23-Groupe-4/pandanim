89f16a400f03d1b4dd5ddeacc9926fd09855e39b
François BANITZ <francois.banitz@gmail.com>
2023-01-05 08:09:49 +0100

    updated database

--- database/pandanim.sql
+++ database/pandanim.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 03 jan. 2023 à 15:38
--- Version du serveur : 8.0.27
--- Version de PHP : 8.0.13
+-- Généré le : jeu. 05 jan. 2023 à 07:08
+-- Version du serveur : 5.7.31
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `pandanim`
 --
+CREATE DATABASE IF NOT EXISTS `pandanim` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pandanim`;
 
 -- --------------------------------------------------------
 
@@ -29,57 +31,50 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `anime`;
 CREATE TABLE IF NOT EXISTS `anime` (
-  `id` int NOT NULL,
-  `image_url` varchar(255) NOT NULL,
-  `small_image_url` varchar(255) NOT NULL,
-  `large_image_url` varchar(255) NOT NULL,
-  `trailer` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `title_english` varchar(255) NOT NULL,
-  `title_japanese` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `episodes` int NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `aired_from` date NOT NULL,
-  `aired_to` date NOT NULL,
-  `duration` varchar(255) NOT NULL,
-  `mal_score` decimal(10,0) NOT NULL,
-  `scored_by` int NOT NULL,
-  `rating` varchar(255) NOT NULL,
-  `synopsis` longtext NOT NULL,
-  `broadcast` varchar(255) NOT NULL,
-  `studios` json NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `id` smallint(5) UNSIGNED NOT NULL COMMENT 'not auto increment -> getted from API',
+  `image_url` varchar(50) DEFAULT NULL,
+  `small_image_url` varchar(52) DEFAULT NULL,
+  `large_image_url` varchar(52) DEFAULT NULL,
+  `trailer` varchar(12) DEFAULT NULL COMMENT 'Youtube id',
+  `title` varchar(150) DEFAULT NULL,
+  `title_english` varchar(150) DEFAULT NULL,
+  `title_japanese` varchar(200) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `episodes` smallint(5) UNSIGNED DEFAULT NULL,
+  `status` varchar(25) DEFAULT NULL,
+  `aired_from` date DEFAULT NULL,
+  `aired_to` date DEFAULT NULL,
+  `duration` varchar(25) DEFAULT NULL,
+  `mal_score` decimal(4,2) DEFAULT NULL COMMENT 'Reviewer score from MyAnimeList (max to 10.00)',
+  `scored_by` int(11) UNSIGNED DEFAULT NULL COMMENT '	Number of reviewer from MyAnimeList',
+  `rating` varchar(50) DEFAULT NULL,
+  `synopsis` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `animegenre`
+-- Structure de la table `anime_genre`
 --
 
-DROP TABLE IF EXISTS `animegenre`;
-CREATE TABLE IF NOT EXISTS `animegenre` (
-  `idAnime` int NOT NULL,
-  `idGenre` int NOT NULL,
-  KEY `idAnime` (`idAnime`),
-  KEY `idGenre` (`idGenre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+DROP TABLE IF EXISTS `anime_genre`;
+CREATE TABLE IF NOT EXISTS `anime_genre` (
+  `anime_id` smallint(5) UNSIGNED NOT NULL,
+  `genre_id` smallint(5) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comments`
+-- Structure de la table `anime_studio`
 --
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `idAnime` int NOT NULL,
-  `idUser` int NOT NULL,
-  `content` longtext NOT NULL,
-  KEY `idAnime` (`idAnime`),
-  KEY `idUser` (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+DROP TABLE IF EXISTS `anime_studio`;
+CREATE TABLE IF NOT EXISTS `anime_studio` (
+  `anime_id` smallint(5) UNSIGNED NOT NULL,
+  `studio_id` smallint(5) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,10 +84,49 @@ CREATE TABLE IF NOT EXISTS `comments` (
 
 DROP TABLE IF EXISTS `genre`;
 CREATE TABLE IF NOT EXISTS `genre` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `id` smallint(5) UNSIGNED NOT NULL COMMENT 'not auto increment -> getted from API',
+  `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE IF NOT EXISTS `review` (
+  `anime_id` smallint(5) UNSIGNED NOT NULL,
+  `user_id` smallint(5) UNSIGNED NOT NULL,
+  `score` tinyint(1) UNSIGNED NOT NULL,
+  `comment` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `review`
+--
+
+INSERT INTO `review` (`anime_id`, `user_id`, `score`, `comment`) VALUES
+(1, 1, 10, 'sqd'),
+(1, 1, 99, 'sqd'),
+(1, 1, 100, 'sqd'),
+(1, 1, 250, 'sqd'),
+(1, 1, 255, 'sqd'),
+(1, 1, 255, 'sqd');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `studio`
+--
+
+DROP TABLE IF EXISTS `studio`;
+CREATE TABLE IF NOT EXISTS `studio` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -102,51 +136,13 @@ CREATE TABLE IF NOT EXISTS `genre` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(128) NOT NULL COMMENT 'SHA-512',
-  `role` json NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `votes`
---
-
-DROP TABLE IF EXISTS `votes`;
-CREATE TABLE IF NOT EXISTS `votes` (
-  `idAnime` int NOT NULL,
-  `idUser` int NOT NULL,
-  KEY `idAnime` (`idAnime`),
-  KEY `idUser` (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `animegenre`
---
-ALTER TABLE `animegenre`
-  ADD CONSTRAINT `animegenre_ibfk_1` FOREIGN KEY (`idAnime`) REFERENCES `anime` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `animegenre_ibfk_2` FOREIGN KEY (`idGenre`) REFERENCES `genre` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`idAnime`) REFERENCES `anime` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `votes`
---
-ALTER TABLE `votes`
-  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`idAnime`) REFERENCES `anime` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(128) NOT NULL COMMENT 'hashed (SHA-512)',
+  `roles` json NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
