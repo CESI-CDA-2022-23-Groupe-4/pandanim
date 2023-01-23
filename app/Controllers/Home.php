9bba5abba022525266e45c452e18c2a86aaad988
Marie <marie.goepp@viacesi.fr>
2023-01-23 15:27:21 +0100

    visu accueil
    visu V1 de l'accueil

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,10 +5,17 @@ use App\Controllers\Anime;
 
 class Home extends BaseController
 {
+    public function __construct()
+    {
+       
+    }
+
     public function index()
     {
         $anime = new Anime();
         $this->_data = $anime->findAllAnimes();
         $this->display('home/index');
     }
+
+    
 }
